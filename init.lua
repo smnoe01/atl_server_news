5546454cff93fd0fa991cab46f5200027c0f19a9
Someone <108891261+smnoe01@users.noreply.github.com>
2025-06-23 16:19:31 +0200

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,151 +1,124 @@
--- Definition des variables
-local atl_server_news = {
-    modpath = minetest.get_modpath("atl_server_news"),
-    mod_storage = minetest.get_mod_storage()
-}
+local modpath = core.get_modpath("atl_server_news")
+local mod_storage = core.get_mod_storage()
+local S = core.get_translator("atl_server_news")
 
--- Traduction (fr, en, it, es, ru)
-local S = minetest.get_translator("atl_server_news")
+core.register_privilege("server_news_edit", {
+    description = "Allows editing of server news",
+    give_to_singleplayer = false
+})
 
-----------
--- Definition des fonctions d'enregistrement
-----------
-
--- Cree le fichier s'il n'existe pas
-function atl_server_news.ensure_news_file_exists()
-    if atl_server_news.mod_storage:get_string("news_content") == "" then
-        atl_server_news.mod_storage:set_string("news_content", "")
-        minetest.log("action", "Created news content in mod_storage.")
-    else
-        minetest.log("action", "News content already exists in mod_storage.")
+local function ensure_storage()
+    if mod_storage:get_string("news_content") == "" then
+        mod_storage:set_string("news_content", "")
+    end
+    if not mod_storage:get_int("news_read_count") then
+        mod_storage:set_int("news_read_count", 0)
     end
 end
 
--- Lit le contenu du fichier
-function atl_server_news.read_news_file()
-    return atl_server_news.mod_storage:get_string("news_content")
+local function read_news()
+    return mod_storage:get_string("news_content")
 end
 
--- Verifie si un joueur a dejà lu les dernieres news 
-function atl_server_news.has_read_news(player_name)
-    local player = minetest.get_player_by_name(player_name)
+local function get_read_count()
+    return mod_storage:get_int("news_read_count")
+end
+
+local function has_read(name)
+    local player = core.get_player_by_name(name)
     if not player then return false end
-    local player_meta = player:get_meta()
-    local last_news_read = player_meta:get_string("last_news_read")
-    local current_news = atl_server_news.read_news_file()
-    return last_news_read == current_news
+    local meta = player:get_meta()
+    return meta:get_string("last_news_read") == read_news()
 end
 
--- Marque les news comme lues pour un joueur
-function atl_server_news.set_news_read(player_name)
-    local player = minetest.get_player_by_name(player_name)
-    if not player then return end
-    local player_meta = player:get_meta()
-    local current_news = atl_server_news.read_news_file()
-    player_meta:set_string("last_news_read", current_news)
-end
-
--- Envoie une notification au joueur s'il y a de nouvelles news
-function atl_server_news.send_news_notification(player_name)
-    minetest.chat_send_player(player_name, S("[ ! ] There are new features on the server! Type /news to read them."))
-end
-
--- Notifie un joueur des nouvelles news s'il ne les a pas encore lues
-function atl_server_news.notify_new_news(player_name)
-    if not atl_server_news.has_read_news(player_name) then
-        atl_server_news.send_news_notification(player_name)
+local function set_read(name)
+    if not has_read(name) then
+        local player = core.get_player_by_name(name)
+        if not player then return end
+        player:get_meta():set_string("last_news_read", read_news())
+        local count = mod_storage:get_int("news_read_count")
+        mod_storage:set_int("news_read_count", count + 1)
     end
 end
 
--- Sauvegarde les nouvelles news
-function atl_server_news.save_news(content)
-    if not content then
-        minetest.log("error", "Content is nil. Cannot save news content.")
-        return
+local function send_notification(name)
+    core.chat_send_player(name, S("[ ! ] New server news available! Type /news to read."))
+end
+
+local function notify_on_join(name)
+    local news = read_news()
+    if news == "" then return end
+    if not has_read(name) then
+        send_notification(name)
     end
-    atl_server_news.mod_storage:set_string("news_content", content)
-    minetest.log("action", "Saved news content in mod_storage.")
 end
 
--- Initialisation fu fichier
-function atl_server_news.init()
-    atl_server_news.ensure_news_file_exists()
+local function save_news(content)
+    if not content then return end
+    mod_storage:set_string("news_content", content)
+    mod_storage:set_int("news_read_count", 0)
+    core.log("action", "News updated; read count reset to 0.")
 end
 
-----------
--- Definition des formspecs
-----------
+local function show_news_form(name)
+    ensure_storage()
+    set_read(name)
 
--- Affiche la fs des news
-function atl_server_news.show_news_form(player_name)
-    atl_server_news.ensure_news_file_exists()
-    local news_content = atl_server_news.read_news_file()
-    local formspec = "size[6,8]" ..
-                     "tabheader[0,0;tabs_news;Server News,Edit server news;1;false;false]" ..
-                     "box[0,0;5.80,8.1;#000000]" ..
-                     "hypertext[0.5,0.15;5.5,8.65;news;<style size=16>" .. minetest.formspec_escape(news_content) .. "</style>]"
-    minetest.show_formspec(player_name, "server_news", formspec)
+    local raw = read_news()
+    local display = raw == "" and S("No news available") or raw
+    local news = core.formspec_escape(display)
+    local count = get_read_count()
+    local is_admin = core.check_player_privs(name, {server_news_edit=true})
+    local tabs = is_admin and "News,Edit News" or "News"
+    local formspec = "size[7,9]" ..
+                     "tabheader[0,0;tabs;" .. tabs .. ";1;false;false]" ..
+                     "box[0,0;6.8,8.75;#000000]" ..
+                     "hypertext[0.5,0.15;5.5,8.65;content;<style size=16>" .. news .. "</style>]" ..
+                     "label[0,8.8;Players who have seen the recent news " .. count .. "]"
+    core.show_formspec(name, "atl_news", formspec)
 end
 
--- Affiche la fs d'edition des news
-function atl_server_news.show_edit_news_form(player_name)
-    local news_content = atl_server_news.read_news_file()
-    local formspec = "size[6,8]" ..
-                     "tabheader[0,0;tabs_news;Server News,Edit server news;2;false;false]" ..
-                     "textarea[0.5,0.5;5.5,7.5;news_content;Edit News:;" .. news_content .. "]" ..
-                     "button[2,7;2,1;save;Save]"
-    minetest.show_formspec(player_name, "server_news_edit", formspec)
+local function show_edit_form(name)
+    local content = read_news()
+    local formspec = "size[7,9]" ..
+                     "tabheader[0,0;tabs;News,Edit News;2;false;false]" ..
+                     "textarea[0.5,0.5;6.5,8.5;news_text;Edit News;" .. core.formspec_escape(content) .. "]" ..
+                     "button[2.5,8;2,1;save_server_news;Save]"
+    core.show_formspec(name, "atl_news_edit", formspec)
 end
 
-----------
--- Definition des commandes
-----------
-
--- Definition de la commande /news
-minetest.register_chatcommand("news", {
+core.register_chatcommand("news", {
     description = "Show server news",
-    privs = {shout = true},
+    privs = {shout=true},
     func = function(name)
-        local player = minetest.get_player_by_name(name)
-        if not player then
-            return false, S("[ ! ] Player does not exist")
-        end
-        atl_server_news.show_news_form(name)
-        if not atl_server_news.has_read_news(name) then
-            atl_server_news.set_news_read(name)
-        end
+        show_news_form(name)
         return true
     end,
 })
 
-----------
--- Definition de la gestion des evenement
-----------
-
--- Gestion des evenements de formulaire
-minetest.register_on_player_receive_fields(function(player, formname, fields)
-    if fields.save then
-        atl_server_news.save_news(fields.news_content)
-        minetest.chat_send_player(player:get_player_name(), S("[ ! ] Server news updated successfully."))
+core.register_on_player_receive_fields(function(player, formname, fields)
+    local name = player:get_player_name()
+    if formname == "atl_news_edit" and fields.save_server_news then
+        save_news(fields.news_text or "")
+        core.chat_send_player(name, S("[ ! ] News updated. Read count reset to 0."))
+        return
     end
-    if fields.tabs_news == "2" then -- Tab: Edit New (Admin Only)
-        if minetest.check_player_privs(player, {server = true}) then
-            atl_server_news.show_edit_news_form(player:get_player_name())
-        else
-            atl_server_news.show_news_form(player:get_player_name())
-            minetest.chat_send_player(player:get_player_name(), S("[ ! ] You don't have permission to open this (missing privileges: server)"))
+    if fields.tabs then
+        if fields.tabs == "1" then
+            show_news_form(name)
+        elseif fields.tabs == "2" then
+            if core.check_player_privs(name, {server_news_edit=true}) then
+                show_edit_form(name)
+            else
+                show_news_form(name)
+                core.chat_send_player(name, S("[ ! ] You lack the server_news_edit privilege."))
+            end
         end
     end
-    if fields.tabs_news == "1" and minetest.check_player_privs(player, {server = true}) then -- Tab: News
-        atl_server_news.show_news_form(player:get_player_name())
-    end
 end)
 
--- Notifie les joueurs des nouvelles news lorsqu'ils rejoignent le serveur
-minetest.register_on_joinplayer(function(player)
-    local player_name = player:get_player_name()
-    atl_server_news.notify_new_news(player_name)
+core.register_on_joinplayer(function(player)
+    notify_on_join(player:get_player_name())
 end)
 
--- Initialisation
-atl_server_news.init()
+ensure_storage()
